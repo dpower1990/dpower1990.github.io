4d978c64f25e17529ba92bcb8ce97b7c70fdc0ce
dpower1990 <36268758+dpower1990@users.noreply.github.com>
2018-04-17 11:55:16 +0100

    Update accordion.js

--- js/accordion.js
+++ js/accordion.js
@@ -10,6 +10,24 @@ if( $('#some-element').css('display')=='none') {
 
 if (is_mobile == true) {
     //Conditional script here
+    var acc = document.getElementsByClassName("Reasons");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
 }
 
 }
